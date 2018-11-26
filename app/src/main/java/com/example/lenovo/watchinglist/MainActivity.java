faf314574e14d7e65eb0004afeaca3d5e4776658
AhmedHassaan <ahmedhassaan162@gmail.com>
2018-11-26 22:17:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/watchinglist/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.lenovo.watchinglist;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import com.example.lenovo.watchinglist.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        adapter = new ArrayAdapter<>(getApplication(),R.layout.mainlistview,new ArrayList<String>(R.array.mainList));
+        binding.mainList.setAdapter(adapter);
+        binding.mainList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent;
+                switch (position){
+                    case 0:
+                        intent = new Intent(getApplicationContext(),WatchingNow.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(getApplicationContext(),NeedToWatch.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        intent = new Intent(getApplicationContext(),Watched.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/watchinglist/NeedToWatch.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.watchinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NeedToWatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_need_to_watch);
+    }
+}
