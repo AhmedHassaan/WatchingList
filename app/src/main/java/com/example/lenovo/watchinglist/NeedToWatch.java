8d6b7989707a5238223b80dd85cd863bd83ac039
AhmedHassaan <ahmedhassaan162@gmail.com>
2018-11-29 00:48:21 +0200

    free of error

--- app/src/main/java/com/example/lenovo/watchinglist/NeedToWatch.java
+++ app/src/main/java/com/example/lenovo/watchinglist/NeedToWatch.java
@@ -1,13 +1,16 @@
 package com.example.lenovo.watchinglist;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NeedToWatch extends AppCompatActivity {
+import com.example.lenovo.watchinglist.databinding.ActivityNeedToWatchBinding;
 
+public class NeedToWatch extends AppCompatActivity {
+    ActivityNeedToWatchBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_need_to_watch);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_need_to_watch);
     }
 }
--- app/src/main/java/com/example/lenovo/watchinglist/Watched.java
+++ app/src/main/java/com/example/lenovo/watchinglist/Watched.java
@@ -1,13 +1,27 @@
 package com.example.lenovo.watchinglist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+import com.example.lenovo.watchinglist.databinding.ActivityWatchedBinding;
 
 public class Watched extends AppCompatActivity {
+    ActivityWatchedBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_watched);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_watched);
+
+//        Log.e("error",id);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.watchedmenu,menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
